6a898bc2ffec7d1711a9d0bbfa2b6d9992bcee0d
Anthony Renwick <renwickman@gmail.com>
2024-10-24 12:20:07 -0500

    First Commit of Forest OOP, No. 2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Forest_OOP__No_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Forest.ForestsCreated);
+
+      Forest f1 = new Forest("Pine", "Desert");
+      Forest f2 = new Forest("Sycamore");
+
+      Console.WriteLine(Forest.ForestsCreated);
+    }
+  }
+}
